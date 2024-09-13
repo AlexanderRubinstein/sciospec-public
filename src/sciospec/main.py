ae605b754b41ba32336f335f7fb2e7c441933811
Alexander Rubinstein <rubalex14@gmail.com>
2024-09-13 19:00:59 +0200

    Add error msg

--- src/sciospec/main.py
+++ src/sciospec/main.py
@@ -56,7 +56,7 @@ class Device:
 
     def assert_execution(self):
         ack_id = self.read_ack()
-        assert ack_id == "83"
+        assert ack_id == "83", "Command has not been executed"
 
     def get_device_id(self):
         # ??
